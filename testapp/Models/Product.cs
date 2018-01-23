8007017ee021ec2405e0189f37f3673a174b806c
Fredrik Olsson <fredriko.olsson@gmail.com>
2018-01-23 11:15:51 +0100

    initial commit

---
+++ testapp/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace testapp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string ArtNum { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Description { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
